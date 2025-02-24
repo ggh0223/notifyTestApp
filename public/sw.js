b0b71c5eab5fd90541dbcb45e11bc8ec914228bb
KimKyuHyun <kim.kyuhyun@lumir@sapce>
2025-02-24 10:14:29 +0900

    sw 수정

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,25 @@
 self.addEventListener('push', (event) => {
+    console.log(event);
     const options = {
       body: event.data.text(),
       icon: '/icon.png',
       badge: '/icon.png',
+      requireInteraction: false,
+      silent: true,
+      priority: 'high',
+      tag: 'lumir',
+      renotify: true,
+      timestamp: Date.now(),
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: 1
+      },
+      actions: [
+        {
+          action: 'explore',
+          title: '자세히 보기'
+        }
+      ]
     };
   
     event.waitUntil(
